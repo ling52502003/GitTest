93d407e06a538ce01bfcaa48dca8329b12bb9586
ling52502003 <ling52502003@gmail.com>
2015-12-26 18:28:21 +0900

    lingqingtest

--- atest_javalianx/src/jp/co/sort/DirectInsertSort.java
+++ atest_javalianx/src/jp/co/sort/DirectInsertSort.java
@@ -5,6 +5,10 @@ public class DirectInsertSort {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 //直接插入排序
+		
+		
+		
+		
 			          int[] a={49,38,65,97,76,13,27,49,78,34,12,64,1};
 			          System.out.println("排序之前：");
 			          for (int i = 0; i < a.length; i++) {
